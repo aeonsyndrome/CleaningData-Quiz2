27757bd63d37bc955c56bdccaaecf7b4ebaaf603
lduplay <leon.duplay@gmail.com>
2015-06-08 10:51:49 +0200

    question2

--- quiz2q2.R
+++ quiz2q2.R
@@ -2,9 +2,9 @@
 library(sqldf)
 
 # Download the file
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv "
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
 download.file(url, destfile= "acs.csv", method = "curl")
 dateDownloaded <- date()
 
 # Read data
-asc <- read.csv("acs.csv")
+asc <- read.csv("getdata-data-ss06pid.csv")
